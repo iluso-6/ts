9c6045c41062da26ad0fd49c7950b672a2000561
Shay de Barra <shaydebarra@gmail.com>
2020-10-13 21:01:32 +0100

    added scroll function to start today btn

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,26 @@
         }
     });
 
+        // Animate to start today button when clicked
+        $('.start-today').click(function(e) {
+
+            // Treat as normal link if no-scroll class
+            if ($(this).hasClass('no-scroll')) return;
+    
+            e.preventDefault();
+            var heading = $(this).attr('href');
+            var scrollDistance = $(heading).offset().top;
+    
+            $('html, body').animate({
+                scrollTop: scrollDistance + 'px'
+            }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+    
+            // Hide the menu once clicked if mobile
+            if ($('header').hasClass('active')) {
+                $('header, body').removeClass('active');
+            }
+        });
+
     // Scroll to top
     $('#to-top').click(function() {
         $('html, body').animate({
